48d175c64fd3c18fd0b1f7515a8e7cab351d44ff
Tihara Egodage <tiharaeg@gmail.com>
2024-08-04 10:27:10 +0530

    initial commit

---
+++ src/app/page.js
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className='text-red-600'>
+        Hellow hammad
+      </div>
+     
+
+    </main>
+  )
+}
