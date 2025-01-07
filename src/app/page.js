d311abcdfabf2c5d2649daf1a463e9610fc802ad
Tihara <tiharaeg@gmail.com>
2025-01-08 01:18:25 +0530

    update HomeCoverSection layout and add Tag component

--- src/app/page.js
+++ src/app/page.js
@@ -3,10 +3,10 @@ import HomeCoverSection from '../components/Home/HomeCoverSection'
 import Header from '../components/header'
 
 export default function Home() {
-  console.log(allBlogs)
     return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className='text-red-600'>
+    // <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="min-h-screen items-center p-24">
+      <div className=''>
         <HomeCoverSection blogs={allBlogs}/>
       </div>
      
--- src/components/Home/HomeCoverSection.js
+++ src/components/Home/HomeCoverSection.js
@@ -1,15 +1,17 @@
 import { sortBlogs } from '@/src/utils.js'
 import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
+import Tag from '../Elements/Tag'
 
 const HomeCoverSection = ({blogs}) => {
 
   const sortedBlogs = sortBlogs(blogs)
   const blog = sortedBlogs[0]
   return (
-    <div className='w-full'>
-      <article className='flex flex-col items-start justify-end mx-10 relative h-[60vh]'>
-        <div className='absolute top-0 left-0 bottom-0 right-0 h-full bg-gradient-to-b from-transparent from-0% to-dark rounded-3xl' />
+    <div className='w-full inline-block'>
+      <article className='flex flex-col items-start justify-end mx-10 relative h-[85vh]'>
+        <div className='absolute top-0 left-0 bottom-0 right-0 h-full bg-gradient-to-b from-transparent from-0% to-dark rounded-3xl z-0' />
       <Image src={blog.image.filePath.replace("../public", "")} 
         placeholder='blur'
         blurDataURL={blog.image.blurhashDataUrl}
@@ -19,7 +21,18 @@ const HomeCoverSection = ({blogs}) => {
         sizes='100vw'
 
       />
-      <div className='w-3/4 p-16 flex flex-col items-start justify-center z-0' />
+      <div className='w-3/4 p-16 flex flex-col items-start justify-center z-0 text-light'>
+        <Tag link={`/categories/${blog.tags[0]}`} name={blog.tags[0]} />
+        <Link href={blog.url} className='mt-6'>
+        <h1 className='font-bold capitalize text-light text-4xl'>
+          {blog.title}
+        </h1>
+        </Link>
+        <p>
+          {blog.description}
+        </p>
+      </div>
+
     </article>
     </div>
 
